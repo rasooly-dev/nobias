0561fa6ecb816dc1b396f34eddc0b96fd747529c
tonynguyen <tonynguyenpersonal@gmail.com>
2023-06-17 21:10:07 -0700

    Added functions for neutral, negative, and positive article generation

--- backend/app/nobias_functions.py
+++ backend/app/nobias_functions.py
@@ -76,3 +76,82 @@ def generate_info(text: str):
 		return json_resp
 	else:
 		raise Exception(f"Error {response.status_code}: {response.text}")
+
+def generate_neutral_article(text: str):
+	prompt = f"Given a piece of text, please generate a concise and unbiased article using the content of the same length." \
+			f" Avoid overly negative words."\
+			f" Also generate a title for the new unbiased objective article."\
+			f" Return the article in a JSON format. Example below:\n"\
+			f"{{\"title\":\"example title\", \"text\":\"Example Unbiased Article\"}}"\
+			f"\n\nHere is the text to generate the unbiased article from:\n{text}"
+	
+	data = {
+		"model": "gpt-4",
+		"messages": [
+			{"role": "system", "content": "You are a helpful assistant."},
+			{"role": "user", "content": prompt}
+		],
+		"max_tokens": 1000
+	}
+
+	response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
+
+	if response.status_code == 200:
+		resp = response.json()["choices"][0]["message"]["content"]
+		json_resp = json.loads(resp)
+		return json_resp
+	else:
+		raise Exception(f"Error {response.status_code}: {response.text}")
+	
+def generate_positive_article(text: str):
+	prompt = f"Given a piece of text, please generate a concise article using the content of the same length." \
+			f" Ensure the article portrays a positive and understanding perspective." \
+			f" Also, generate a title for the new article that highlights the positive aspects." \
+			f" Return the article in JSON format. Example below:\n" \
+			f"{{\"title\":\"example title\", \"text\":\"Example Unbiased Article\"}}\"" \
+			f"\n\nHere is the text to generate the unbiased article from:\n{text}"
+	
+	data = {
+		"model": "gpt-4",
+		"messages": [
+			{"role": "system", "content": "You are a helpful assistant."},
+			{"role": "user", "content": prompt}
+		],
+		"max_tokens": 1000
+	}
+
+	response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
+
+	if response.status_code == 200:
+		resp = response.json()["choices"][0]["message"]["content"]
+		json_resp = json.loads(resp)
+		return json_resp
+	else:
+		raise Exception(f"Error {response.status_code}: {response.text}")
+	
+def generate_negative_article(text: str):
+	prompt = f"Given a piece of text, please generate a concise article using the content of the same length." \
+         f" Provide a negative perspective that acknowledges flaws influencing the topic." \
+		 f" Offer negative viewpoints critiquing the subject" \
+         f" Also, generate a title for the new article that highlights a nuanced viewpoint." \
+         f" Return the article in JSON format. Example below:\n" \
+         f"{{\"title\":\"example title\", \"text\":\"Example Unbiased Article\"}}\"" \
+         f"\n\nHere is the text to generate the article from:\n{text}"
+
+	data = {
+		"model": "gpt-4",
+		"messages": [
+			{"role": "system", "content": "You are a helpful assistant."},
+			{"role": "user", "content": prompt}
+		],
+		"max_tokens": 1000
+	}
+
+	response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
+
+	if response.status_code == 200:
+		resp = response.json()["choices"][0]["message"]["content"]
+		json_resp = json.loads(resp)
+		return json_resp
+	else:
+		raise Exception(f"Error {response.status_code}: {response.text}")
