3c4062026b0836c2852d8ef96c4532c007961e00
tonynguyen <tonynguyenpersonal@gmail.com>
2023-06-17 22:39:47 -0700

    added slashes to endpoints (oops)

--- backend/app/nobias_api.py
+++ backend/app/nobias_api.py
@@ -17,17 +17,17 @@ async def generate_info_endpoint(text: str):
     json_resp = generate_info(text)
     return json_resp
 
-@app.get("generate_negative_article")
+@app.get("/generate_negative_article")
 async def generate_negative_article(text: str):
     json_resp = generate_negative_article(text)
     return json_resp
 
-@app.get("generate_neutral_article")
+@app.get("/generate_neutral_article")
 async def generate_neutral_article(text: str):
     json_resp = generate_neutral_article(text)
     return json_resp
 
-@app.get("generate_positive_article")
+@app.get("/generate_positive_article")
 async def generate_positive_article(text: str):
     json_resp = generate_positive_article(text)
     return json_resp
